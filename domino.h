c65c98ed71c18a18644bdbe3475d34f42d3e3ea8
Leonardo Grupioni <52112271+leonardogrupioni@users.noreply.github.com>
2022-08-09 21:47:24 -0300

    Update domino.h

--- domino.h
+++ domino.h
@@ -1,11 +1,13 @@
 #define TOTALPECAS 28
 
+
 typedef struct _peca{
     int l1; //lado 1
     int l2; //lado 2
+    //int id; //id de 0 a 27;
+    int posicao; //posicao da peca
+    char stats; //onde esta a peca, P = pilha, 1 = jogador1, 2 = jogador2, M = mesa
 } PECAS;
 
 void inicializarPecas(PECAS *pecas);
 void imprimirTodasPecas(PECAS *pecas);
-void embaralharPecas(PECAS *pecas);
-void sorteio(int qtd, int max, int retorno[qtd]);
