831f789b611470365de6eab0147064b8dec7018e
Leonardo Grupioni <52112271+leonardogrupioni@users.noreply.github.com>
2022-08-09 21:47:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "domino.h"
+#include "mudaPosicao.h"
 
 int main(){
     //funcao para gerar sempre um novo numero randomico
@@ -11,12 +12,26 @@ int main(){
 
     //criando array pecas:
     PECAS pecas[28];
-    
+
     inicializarPecas(pecas);
 
     imprimirTodasPecas(pecas);
 
     embaralharPecas(pecas);
 
+    desembaralharPeca(pecas);
+
+    /*
+    //vendendo as 7 primeiras peças
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    for(int i = 0; i < 7; i++){
+        pecas[i].stats = '1';
+        printf("Peca entregue #%d: %d / %d", i+1, pecas[pecas[i].posicao].l1, pecas[pecas[i].posicao].l2);
+        printf("\n");
+    }
+
+    */ //ideia para o futuro do projeto
     return 0;
 }
