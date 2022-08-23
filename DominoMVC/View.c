3cad199daa99ffb21400c3748a522dd540d10ba4
Leonardo Grupioni <52112271+leonardogrupioni@users.noreply.github.com>
2022-08-22 21:25:16 -0300

    Update View.c

--- DominoMVC/View.c
+++ DominoMVC/View.c
@@ -9,7 +9,7 @@
 
 
 
-void imprimirTodasPecas(PECAS pecas[]){
+void imprimirTodasPecas(PECA pecas[]){
     printf("Pecas do Domino\nTotal: 28\n\n");
     printf("          Pecas\n");
     printf("N-daPeca Lado 1 | Lado 2\n");
@@ -18,24 +18,68 @@ void imprimirTodasPecas(PECAS pecas[]){
     }
 }
 
-void imprimirPecasEmbaralhadas(PECAS pecas[]){
+void imprimirPecasEmbaralhadas(PECA pecas[]){
     
     printf("\nAs 28 pecas embaralhadas sao: \n");
     for(int i = 0; i < 28; i++){
+        //possibilidade de transformar em funcao, funcao para imprimir peca, passando como parametro a peca e a posicao
         printf("Peca aleatoria #%d: %d | %d\n", i+1, pecas[pecas[i].posicao].l1, pecas[pecas[i].posicao].l2);
     }
 }
 
-void imprimirPecasDesembaralhadas(PECAS pecas[]){
+void imprimirPecasDesembaralhadas(PECA pecas[]){
     printf("\nAs 28 pecas desembaralhadas sao: \n");
     for(int i = 0; i < 28; i++){
         printf("Peca #%d: %d | %d\n", pecas[i].posicao+1, pecas[i].l1, pecas[i].l2);
     }
 }
 
-int imprimirMenuInicial(){
-    int opcao;
-    printf("Escolha a opcao desejada: \n[1]inicializa\n[2]imprime pecas\n[3]embaralha e imprime\n[4]desembaralha e imprime\n[5]sair\n...");
-    scanf("%d", &opcao);
-    return opcao;
+int escolher() {
+    int escolha;
+    scanf("%d", &escolha);
+    return escolha;
+}
+
+void imprimirInterface() {
+
+    printf("\n==============================\n");
+    printf("(1) - Jogar\n");
+    printf("(2) - Retomar Partida\n");
+    printf("(3) - Salvar Partida\n");
+    printf("(4) - Carregar Partida Salva\n");
+    printf("(5) - Regras\n");
+    printf("(0) - Sair\n");
+    printf("==============================\n");
+    printf("Escolha sua opcao desejada: ");
+
+}
+
+void imprimirJogarMenu(){
+    printf("\n==============================\n");
+    printf("(1) - Dois jogadores\n");
+    printf("(2) - Contra a IA\n");
+    printf("==============================\n");
+    printf("Escolha sua opcao desejada: ");    
+}
+
+void opcaoEscolhaInvalida() {
+    printf("Sua opcao e invalida! Tente novamente \n");
+}
+
+void iniciarJogo() {
+    printf("JOGO DE DOMINO!\n");
+}
+
+//funcao temporaria para indicar paginas de futuras etapas
+void emDesenvolvimento(){
+    printf("AVISO:\nEsta pagina ainda esta em desenvolvimento\nSera feita nas proximas etapas do projeto\n");
+}
+
+void sairDoJogo(){
+    printf("Sair");
+}
+
+void jogadorInicial(int jogador){
+    printf("O jogador %c comeca jogando\n", (jogador==1?'1':'2')); //uso do if iternario (se <condicao> <?> for verdadeira entao '<1opcao>' senão <:> '<2opcao>')
+
 }
